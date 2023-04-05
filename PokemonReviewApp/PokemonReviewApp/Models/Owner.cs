0d5a4ce9ba2d86f38e03aac7003408bdd9d66752
Philip Abangan <philip.abangan@aon.com>
2023-04-05 13:18:58 -0500

    Added entity relationships

--- PokemonReviewApp/PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/PokemonReviewApp/Models/Owner.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public string Gym { get; set; }
         public Country Country { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+
     }
 }
