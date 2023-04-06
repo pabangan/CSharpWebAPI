000d30fa157b8fd9ee5e798d1d638906dd26899f
Philip Abangan <philip.abangan@aon.com>
2023-04-06 14:30:17 -0500

    Got automapper to work. Updated db to from pokemons to pokemon

--- PokemonReviewApp/PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/PokemonReviewApp/Controllers/PokemonController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interfaces;
 using PokemonReviewApp.Models;
+using PokemonReviewApp.Repository;
 using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace PokemonReviewApp.Controllers
@@ -14,22 +17,59 @@ namespace PokemonReviewApp.Controllers
     public class PokemonController : Controller
     {
         private readonly IPokemonRepository _pokemonRepository;
-        public PokemonController(IPokemonRepository pokemonRepository)
+        private readonly IMapper _mapper;
+
+        public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
         {
             this._pokemonRepository = pokemonRepository;
+            this._mapper = mapper;
         }
 
         [HttpGet]
         [ProducesResponseType(200, Type=typeof(IEnumerable<Pokemon>))]
         public IActionResult GetPokemons()
         {
-            var pokemon = _pokemonRepository.GetPokemons();
+            var pokemon = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
 
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
             return Ok(pokemon);
+        }
+
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200, Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId)
+        {
+            if(!_pokemonRepository.PokemonExists(pokeId))
+            {
+                return NotFound();
+            }
+            var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokeId));
+            if(!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(pokemon);
+        }
+
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200, Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if(!_pokemonRepository.PokemonExists(pokeId))
+            {
+                return NotFound();
+            }
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            return Ok(rating);
         }
     }
 }
--- PokemonReviewApp/PokemonReviewApp/Program.cs
+++ PokemonReviewApp/PokemonReviewApp/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using PokemonReviewApp;
 using PokemonReviewApp.Data;
 using PokemonReviewApp.Interfaces;
@@ -9,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
